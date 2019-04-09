be6638ab5dec98413d897cd39602c875d955821f
nvvar <nwar0825@users.noreply.github.com>
2019-04-09 14:19:36 +0900

    commit

---
+++ app/src/main/java/com/nwar/individual/mvp/MVP/View/RecyclerViewActivity.kt
@@ -0,0 +1,13 @@
+package com.nwar.individual.mvp.MVP.View
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.nwar.individual.mvp.R
+
+class RecyclerViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+    }
+}
